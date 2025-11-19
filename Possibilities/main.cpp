2e38e255968e4d88728a372817711b365c7be6b9
Doğukan <servicesnoma@proton.me>
2025-11-19 16:13:17 +0300

    Initial Commit

---
+++ Possibilities/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Possibilities
+//
+//  Created by Feyza Çakmaklı on 19.11.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
